c208a7c4a1c48ffd7ee6f80537a2eae13d1afbaa
Kate Hynes <kahynes@davidson.edu>
2023-03-09 18:25:31 -0500

    chapter 4

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -20,7 +20,46 @@ public class LearningCurve : MonoBehaviour
         Debug.Log($"{myString} world!");
         string concatString = "Hello" + " world.";
         Debug.Log(concatString);
+
+        // ch 4
+        if(myInt == 2) {
+            Debug.Log("Just right!");
+        }
+        else if(myInt - 1 == 2 || myInt + 1 == 2) {
+            Debug.Log("So close!");
+        }
+        else {
+            if(myInt < 2) {
+                Debug.Log("Too low!");
+            }
+            else {
+                Debug.Log("Too high!");
+            }
+        }
+
+        string response = "Yes";
+        switch(response) {
+            case "Yes":
+            Debug.Log("Great, let's take a look!");
+            break;
+            case "No":
+            Debug.Log("Okay, let's move on.");
+            break;
+        }
+
+        int[] myArr = new int[] {1, 2, 3, 4, 5};
+        List<string> stringList = new List<string> {"Kate", "Hynes"};
+        Dictionary<string, int> points = new Dictionary<string, int> {{"P1", 4}, {"P2", 5}};
         
+        for (int i = 0; i < 2; i++) {
+            Debug.LogFormat("index: {0} - {1}", i, stringList[i]);
+        }
+        foreach(string name in stringList) {
+            Debug.LogFormat("name: {0}", name);
+        }
+        foreach(KeyValuePair<string, int> kvp in points) {
+            Debug.LogFormat("points: {0} - {1}", kvp.Key, kvp.Value);
+        }
     }
 
     // Update is called once per frame
