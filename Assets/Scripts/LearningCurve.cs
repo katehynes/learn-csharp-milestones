f83d7470ca072a2c4484e0b1e7f7219612ab23dc
Kate Hynes <kahynes@davidson.edu>
2023-03-09 18:05:57 -0500

    chapter 2

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int myInt = 0;
+    public float myFloat = 3.0f;
+    public string myString = "Hello";
+    public bool myBool = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // single line comment
+        int doubled = DoubleInt(myInt);
+        /* this is
+        a multi line comment*/
+        Debug.Log(myInt);
+        Debug.LogFormat("This is myInt doubled: {0}", doubled);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    /// <summary>
+    /// Doubles an integer.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public int DoubleInt(int x) {
+        return 2 * x;
+    }
+}
